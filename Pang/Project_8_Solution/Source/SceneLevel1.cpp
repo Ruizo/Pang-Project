f383725b782d78922c102a12ad8de4900abf013e
Makinilla-maker <valverdeoriol@gmail.com>
2020-04-21 11:20:54 +0200

    Pang with Intro Sceen
    Pang with Intro Sceen

---
+++ Pang/Project_8_Solution/Source/SceneLevel1.cpp
@@ -0,0 +1,73 @@
+#include "SceneLevel1.h"
+
+#include "Application.h"
+#include "ModuleTextures.h"
+#include "ModuleRender.h"
+#include "ModuleAudio.h"
+#include "ModuleCollisions.h"
+#include "ModuleEnemies.h"
+#include "ModulePlayer.h"
+
+SceneLevel1::SceneLevel1(bool startEnabled) : Module(startEnabled)
+{
+
+}
+
+SceneLevel1::~SceneLevel1()
+{
+
+}
+
+// Load assets
+bool SceneLevel1::Start()
+{
+	LOG("Loading background assets");
+
+	bool ret = true;
+
+	bgTexture = App->textures->Load("Assets/Sprites/background.png");
+	App->audio->PlayMusic("Assets/Music/stage1.ogg", 1.0f);
+
+	App->collisions->AddCollider({ 0, 0, 384, 10 }, Collider::Type::WALL);
+	App->collisions->AddCollider({ 0, 231, 384, 9 }, Collider::Type::WALL);
+	App->collisions->AddCollider({ 0, 10, 9, 221 }, Collider::Type::WALL);
+	App->collisions->AddCollider({ 375, 10, 9, 221 }, Collider::Type::WALL);
+
+	// Enemies ---
+	App->enemies->AddEnemy(Enemy_Type::Balls, 168, 120);
+
+
+	App->render->camera.x = 0;
+	App->render->camera.y = 0;
+
+	// TODO 2: Enable (and properly disable) the player module
+	App->player->Enable();
+	App->enemies->Enable();
+
+	return ret;
+}
+
+Update_Status SceneLevel1::Update()
+{
+	return Update_Status::UPDATE_CONTINUE;
+}
+
+// Update: draw background
+Update_Status SceneLevel1::PostUpdate()
+{
+	// Draw everything --------------------------------------
+	App->render->Blit(bgTexture, 0, 0, NULL);
+
+	return Update_Status::UPDATE_CONTINUE;
+}
+
+bool SceneLevel1::CleanUp()
+{
+	// TODO 2: Enable (and properly disable) the player module
+	App->player->Disable();
+	App->enemies->Disable();
+
+	// TODO 5: Remove All Memory Leaks - no solution here guys ;)
+
+	return true;
+}
